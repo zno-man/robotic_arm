e777474357280d929667c9645aaff450b47d842e
EBIN JOHNY <ebinchemp@gmail.com>
2022-04-18 12:47:42 +0530

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,12 +1,18 @@
 
 def configure():
 	print("Enter loc 1 cordinates:",end = '')
-	t1 = list(map(float , input().strip().split()))
+	lo1 = list(map(float , input().strip().split()))
 	print("Enter loc 2 cordinates:",end = '')
-	t2 = list(map(float , input().strip().split()))
+	lo2 = list(map(float , input().strip().split()))
 	print("Enter loc 3 cordinates:",end = '')
-	t3 = list(map(float , input().strip().split()))
-	print(t1,t2,t3)
+	lo3 = list(map(float , input().strip().split()))
+	print(lo1,lo2,lo3)
+	return lo1,lo2,lo3
+
+
+lo1 = [0,0,0]
+lo2 = [0,0,0]
+lo3 = [0,0,0]
 
 while(True):
 	print("""
@@ -17,20 +23,32 @@ while(True):
         2) goto l1
         3) goto l2
         4) goto l3
-        5) exit
+        5) pick up
+        6) collect
+        7) deposit
+        8) adjust angles
+        9) exit
               """)
 	print(":",end = "")
         
 	inp = input()
 	if inp == "1":
-		configure()
+		lo1,lo2,lo3 = configure()
 	elif inp == "2":
-		a = 1
+		pass
 	elif inp == "3":
-		a = 1
+		pass
 	elif inp == "4":
-		a = 1
+		pass
 	elif inp == "5":
+		pass
+	elif inp == "6":
+		pass
+	elif inp == "7":
+		pass
+	elif inp == "8":
+		pass
+	elif inp == "9":
 		break
 
 print("exiting menu...")
