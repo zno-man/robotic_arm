81ceabe7f2acbbce969a1f95c5c11a068ffa8765
EBIN JOHNY <ebinchemp@gmail.com>
2022-04-18 17:58:56 +0530

    new model control

--- control.py
+++ control.py
@@ -1,73 +1,43 @@
-
-
+#!/usr/bin/env python3
 import rospy
+from std_msgs.msg import Float64
 import math
 
-
-from std_msgs.msg import Float64
-
 rospy.init_node('control')
-pub = rospy.Publisher('/robot_arm_iteration_3/Rev13_position_controller/command', Float64, queue_size=10)
-pub2 = rospy.Publisher('/robot_arm_iteration_3/Rev12_position_controller/command', Float64, queue_size=10)
-pub3 = rospy.Publisher('/robot_arm_iteration_3/Rev11_position_controller/command', Float64, queue_size=10)
-pub4 = rospy.Publisher('/robot_arm_iteration_3/Rev10_position_controller/command', Float64, queue_size=10)
-r = rospy.Rate(50)
+end_effector = 0
+lnk3 = 0
+lnk2 = 0
+lnk1 = 0
+end_tip = 0
+end_slide_vel = 0 #can be -0.1(deploy) or 0.1(retract)
+vel = [0.1,-0.1]
 
-initial_angle=math.radians(0)
+pub_end_effector = rospy.Publisher('/robot_arm_iteration_3/Rev10_position_controller/command', Float64, queue_size=1)
+pub_lnk3 = rospy.Publisher('/robot_arm_iteration_3/Rev11_position_controller/command', Float64, queue_size=1)
+pub_lnk2 = rospy.Publisher('/robot_arm_iteration_3/Rev12_position_controller/command', Float64, queue_size=1)
+pub_lnk1 = rospy.Publisher('/robot_arm_iteration_3/Rev13_position_controller/command', Float64, queue_size=1)
+pub_end_tip = rospy.Publisher('/robot_arm_iteration_3/Rev15_position_controller/command', Float64, queue_size=1)
+pub_end_slide_vel = rospy.Publisher('/robot_arm_iteration_3/Slider14_position_controller/command', Float64, queue_size=1)
 
+count = 0
+bool = True
 
-def oscilation(pub_name,ang):
-	temp=0
-	while not rospy.is_shutdown():
-		angle=temp
-		if temp>ang:
-			angle = 2*ang-temp
-		angle = math.radians(angle)
-		pub_name.publish(angle)
-		temp=temp+1
-		if temp>2*ang:
-			temp=0
-		r.sleep()
+r = rospy.Rate(100)
 
-def one_time(pub_name,ang):
-	temp=0
-	while not rospy.is_shutdown():
-		angle = temp
-		angle = math.radians(angle)
-		pub_name.publish(angle)
-		temp=temp+1
-		if temp>ang:
-			break
-		r.sleep()
-
-def return_to_initial_stage():
-	pub.publish(initial_angle)
-	pub2.publish(initial_angle)
-	pub3.publish(initial_angle)
-	pub4.publish(initial_angle)	
-		
-#print(type(pub))
-"""
 while not rospy.is_shutdown():
-    #angle = rospy.get_time() % 360
-    angle = temp
-    if temp>60:
-    	angle=120-temp
+    print(":",end = '')
+    angle = math.radians(int(input()))
+    # angle = rospy.get_time() % 360 
+    # pub_end_effector.publish(angle)
+    # pub_lnk3.publish(angle)
+    pub_lnk2.publish(angle)
+    # pub_lnk1.publish(angle)
+    # pub_end_tip.publish(angle)
     
-    angle=math.radians(angle)
-    pub.publish(angle)
-    pub2.publish(angle)
-    pub3.publish(angle)
-    pub4.publish(angle)    
-    temp=temp+1
-    if temp>120:
-    	temp=0
-    	"""
-
-one_time(pub,60)
-one_time(pub2,29)
-one_time(pub4,100)
-return_to_initial_stage()
-
-
-    #r.sleep()
+    # count+=1
+    # if count == 100:
+    #     pub_end_slide_vel.publish(vel[int(bool)])
+    #     bool = not bool
+    #     count = 0
+        
+    r.sleep()
