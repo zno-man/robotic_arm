2d53dba38baddbb57b5fdbac0a19e8dc8130802b
EBIN JOHNY <ebinchemp@gmail.com>
2022-04-24 17:31:23 +0530

    slider in flask app

--- control_app.py
+++ control_app.py
@@ -1,17 +1,41 @@
-from ast import AsyncFunctionDef
-from flask import Flask
-
+from flask import Flask #pip install flask (or) sudo apt install python3-flask
+from flask_cors import CORS #pip install -U flask-cors 
+ 
 app = Flask(__name__)
-
+CORS(app)
+ 
+ 
 @app.route("/")
-def home():
-    return "Hello, World!"
-    
-@app.route("/salvador")
-def salvador():
-    return "Hello, Salvador"
-    
+def helloSlider():
+    return '''
+<html>
+ 
+<body>
+ 
+ 
+<div class="slidecontainer">
+  <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+  <p>Value: <span id="demo"></span></p>
+</div>
+ 
+<script>
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+ 
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+ 
+ 
+</script>
+ 
+</body>
+</html>
+'''
+ 
+ 
+ 
+ 
 if __name__ == "__main__":
     app.run(debug=True)
-    
-
